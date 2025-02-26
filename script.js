13341807f3b09b449ff35f9e570174741c76699f
Stephen Raj V.A <stephenrajva@gmail.com>
2025-02-26 16:38:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.read-more').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Read more functionality will be implemented soon!');
+    });
+});
